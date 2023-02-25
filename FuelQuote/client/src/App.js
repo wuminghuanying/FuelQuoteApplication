5f5a5585baea227bb2a26ec404d07b19cb400ca1
Wei Jiang Lin Tan <lintanweiqiang@yahoo.com>
2023-02-24 20:23:28 -0600

    add Fuel Quote History

--- FuelQuote/client/src/App.js
+++ FuelQuote/client/src/App.js
@@ -5,6 +5,7 @@ import CPM from './components/CPM/cpm.js';
 import FuelPrice from './components/FuelPrice/fuel';
 import LOGIN from './components/Login/login.js';
 import REGISTER from './components/Register/register.js';
+import HISTORY from './components/FuelQuoteHistory/history.js';
 
 
 
@@ -19,6 +20,7 @@ function App() {
         <Route  path="/cpm" element={<CPM/>} />
         <Route  path="/login" element={<LOGIN/>} />
         <Route  path="/register" element={<REGISTER/>} />
+        <Route  path="/history" element={<HISTORY/>} />
 
       </Routes>
 
---
+++ FuelQuote/client/src/components/FuelQuoteHistory/history.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./history.css"
+
+const history = () => {
+
+    return (
+        
+        <div className="form">
+            
+            <table className ='form'>
+  <tr>
+    <th>Gallons Requested|</th>
+    <th>Address|</th>
+    <th>|Country|</th>
+    <th>City|</th>
+    <th>State|</th>
+    <th>Zip code|</th>
+    <th>Data|</th>
+    <th>Suggested price|</th>
+  </tr>
+  
+</table>
+
+        </div>
+    )
+}
+
+export default history;

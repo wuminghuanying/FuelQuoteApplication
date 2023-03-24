322573a74e80efc40cb42ea0c6f771c4db089dbd
wuminghuanying <lintanweiqiang@yahoo.com>
2023-03-24 18:43:06 -0500

    add backend for register

--- FuelQuote/server/index.js
+++ FuelQuote/server/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cpmRoutes from "./routes/cpmRoutes.js";
+import RegisterRoutes from "./routes/RegisterRoutes.js";
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", cpmRoutes);
+app.use("/api", RegisterRoutes);
 
 
-app.listen(5500, () => { });
+app.listen(6100, () => { });
 
 export default app;

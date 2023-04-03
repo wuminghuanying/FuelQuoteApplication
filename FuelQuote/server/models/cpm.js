d2451f8ac4a6d941c090d8e82c8c20dddba7c576
arsh <70237949+arsh786110@users.noreply.github.com>
2023-04-02 21:37:25 -0500

    updated

---
+++ FuelQuote/server/models/cpm.js
@@ -0,0 +1,28 @@
+//Full_name, address1, address2, city, state, zipcode
+import mongoose from "mongoose";
+const cpmSchema = new mongoose.Schema({
+    Full_name: {
+        type: Number,
+        required: true,
+    },
+    address1: {
+        type: String,
+        required: true,
+    },
+    address2: {
+        type: String,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        required: true,
+    },
+    
+})

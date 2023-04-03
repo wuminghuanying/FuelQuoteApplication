b1ba1804df2a1f5fbe7466b81e1e4f93442d7149
arsh <70237949+arsh786110@users.noreply.github.com>
2023-04-02 20:48:16 -0500

    done with connection

--- FuelQuote/server/index.js
+++ FuelQuote/server/index.js
@@ -3,6 +3,17 @@ import cpmRoutes from "./routes/cpmRoutes.js";
 import fuelpriceRoutes from "./routes/fuelpriceRoutes.js";
 import RegisterRoutes from "./routes/RegisterRoutes.js";
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+
+
+const connect= async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+    } catch (error) {
+        throw error
+    }
+};
 
 dotenv.config();
 const app = express();
@@ -16,5 +27,7 @@ app.use("/api", RegisterRoutes);
 
 if (process.env.NODE_ENV !== "test") {
     app.listen(process.env.PORT || 5500);
+    connect()
+    console.log("Connected to Server"); 
 }
 export default app;
